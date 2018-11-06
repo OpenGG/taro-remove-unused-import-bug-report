1c4ab3255c40a8a1d9651bced0054045677af7f7
opengg <liy099@gmail.com>
2018-11-06 15:53:52 +0800

    init

---
+++ src/lib/constants.ts
@@ -0,0 +1,2 @@
+export const BAIDU = 'BAIDU'
+export const GOOGLE = 'GOOGLE'
---
+++ src/lib/host.ts
@@ -0,0 +1,6 @@
+import { BAIDU, GOOGLE } from "./constants";
+
+export const HOST = {
+  [BAIDU]: '//www.baidu.com',
+  [GOOGLE]: '//www.google.com'
+};
